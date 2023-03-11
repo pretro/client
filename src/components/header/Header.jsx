31d042ab2fec6ccfb4c80822c78c1c47cb0ef0b2
jort2 <>
2023-03-11 22:30:16 +0100

    creating the category folder

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import logo from "../../assets/images/logo.png"
+import { nav } from "../../assets/data/data"
+import { Link } from "react-router-dom"
+import { User } from "./User"
+import "./header.css"
 
 const Header = () => {
   return (
@@ -9,7 +13,19 @@ const Header = () => {
           <div className='logo'>
             <img src={logo} alt='logo' width='100px' />
           </div>
+          <nav>
+          <ul>
+            {nav.map((link) => (
+              <li key={link.id}>
+                <Link to={link.url}>{link.text}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
         </div>
+        <div className='account flexCenter'>
+        <User />
+      </div>
       </header>
     </>
   )
